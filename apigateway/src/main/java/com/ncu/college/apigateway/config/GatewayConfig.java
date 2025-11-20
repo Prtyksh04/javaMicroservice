4f496e1e37c17a421fa2e78f18bc4af8ee0b91d6
pratyaksh <pratyaksh22csu447@ncuindia.edu>
2025-11-20 17:55:04 +0530

    Added config server

---
+++ apigateway/src/main/java/com/ncu/college/apigateway/config/GatewayConfig.java
@@ -0,0 +1,24 @@
+package com.ncu.college.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("userservice", r -> r.path("/user/**")
+                        .uri("lb://userservice"))
+                .route("movieservice", r -> r.path("/movie/**")
+                        .uri("lb://movieservice"))
+                .route("ticketbookingservice", r -> r.path("/ticket/**")
+                        .uri("lb://ticketbookingservice"))
+                .build();
+    }
+
+}
--- configurationserver/src/main/java/com/ncu/college/configurationserver/ConfigurationserverApplication.java
+++ configurationserver/src/main/java/com/ncu/college/configurationserver/ConfigurationserverApplication.java
@@ -2,10 +2,10 @@ package com.ncu.college.configurationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigurationServer;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigurationServer
+@EnableConfigServer
 public class ConfigurationserverApplication {
 
 	public static void main(String[] args) {
